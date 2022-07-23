6de7642cc60c76999b28bdb2d172af17e2d65e31
deimosgo <davinsider345@gmail.com>
2022-07-23 03:38:57 -0500

    fixCompAgainTwo

--- src/main.js
+++ src/main.js
@@ -5,16 +5,12 @@ import App from './App.vue';
 import router from './router';
 import VueHtmlToPaper from './plugins/VueHtmlToPaper';
 
-let basePath= 'http://localhost:8080';
 const options = {
     name: '',
     specs: [
         'fullscreen=yes',
         'titlebar=yes',
         'scrollbars=yes'
-    ],
-    styles: [
-        `${basePath}/comp.css`
     ],
     timeout: 1000, // default timeout before the print window appears
     autoClose: true, // if false, the window will not close after printing
